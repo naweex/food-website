0f1fce6d8d9b1d8f31e129f5b6667bbf43324001
navid bahrami <navidbahrami1996@gmail.com>
2025-02-21 12:54:20 +0330

    typeorm config

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,25 @@
+import {TypeOrmModuleOptions} from "@nestjs/typeorm";
+import {config} from "dotenv";
+import {join} from "path";
+
+config();
+config({
+  path: join(process.cwd(), `.env.${process.env.NodeEnv}`),
+});
+export function TypeOrmConfig(): TypeOrmModuleOptions {
+  const {DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME} = process.env;
+  return {
+    type: "mysql",
+    host: DB_HOST,
+    port: DB_PORT,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    autoLoadEntities: false,
+    synchronize: true,
+    entities: [
+      "dist/**/**/**/*.entity{.ts,.js}",
+      "dist/**/**/*.entity{.ts,.js}",
+    ],
+  };
+}
