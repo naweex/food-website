ee260087d26263a1b73c986b6253e7751c263902
navid bahrami <navidbahrami1996@gmail.com>
2025-02-22 15:23:11 +0330

    add some functions

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,28 +1,53 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CategoryEntity } from './entities/category.entity';
 import { Repository } from 'typeorm';
+import { S3Service } from '../s3/s3.service';
+import { isBoolean, toBoolean } from 'src/common/utility/function.utils';
 
 @Injectable()
 export class CategoryService {
-  constructor(@InjectRepository(CategoryEntity) private categoryRepository:Repository<CategoryEntity>){}
-  create(createCategoryDto: CreateCategoryDto, image: Express.Multer.File) {
-    
+  constructor(@InjectRepository(CategoryEntity) private categoryRepository:Repository<CategoryEntity> ,
+  private s3Service : S3Service
+){}
+  async create(createCategoryDto: CreateCategoryDto, image: Express.Multer.File) {
+    const {Location} = await this.s3Service.uploadFile(image , 'food-website-image')//create food-website-image file and uload images into there.
+    let {parentId , show , slug , title } = createCategoryDto
+    const category = await this.findOneBySlug(slug)
+    if(category) throw new ConflictException('category already existed')
+    if(isBoolean(show)) {
+      show = toBoolean(show);
+    }
+    let parent : CategoryEntity = null;
+    if(parentId && !isNaN(parentId)){
+      parent = await this.findOneById(+parentId)
+    }
+    await this.categoryRepository.insert({
+      title ,
+      slug ,
+      show ,
+      image : Location ,
+      parentId : parent?.id,
+  })
+  return {
+    message : ' category created successfully'
+    }
   }
 
   findAll() {
     return `This action returns all category`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  async findOneById(id: number) {
+    const category = await this.categoryRepository.findOneBy({id})
+    if (!category) throw new NotFoundException('category not found')
+      return category;
   }
   
   async findOneBySlug(slug: string) {
-    const category = await this.categoryRepository.findOneBy({slug})
-    if(category) throw new ConflictException('category already existed')
+    return await this.categoryRepository.findOneBy({slug})
   }
 
   update(id: number, updateCategoryDto: UpdateCategoryDto) {
